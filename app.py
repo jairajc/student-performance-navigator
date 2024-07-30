db73e30df9d7caef14524b63ff118b37aa1f0ec9
Jai Raj Choudhary <jairaj.c9@gmail.com>
2024-07-30 15:46:37 -0500

    flask app updated with modification in the home page.

--- app.py
+++ app.py
@@ -22,26 +22,30 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+        try:
+            data=CustomData(
+                gender=request.form.get('gender'),
+                race_ethnicity=request.form.get('ethnicity'),
+                parental_level_of_education=request.form.get('parental_level_of_education'),
+                lunch=request.form.get('lunch'),
+                test_preparation_course=request.form.get('test_preparation_course'),
+                reading_score=float(request.form.get('writing_score')),
+                writing_score=float(request.form.get('reading_score'))
 
-        )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
+            )
+            pred_df = data.get_data_as_data_frame()
+            print(pred_df)
+            print("Before Prediction")
 
-        predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
-        return render_template('home.html',results=results[0])
-    
+            predict_pipeline = PredictPipeline()
+            print("Mid Prediction")
+            results = predict_pipeline.predict(pred_df)
+            print("After Prediction")
+            return render_template('home.html', results=results[0])
+            
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            return render_template('error.html', error_message=str(e))
 
-if __name__=="__main__":
-    app.run(port=5001)    
+if __name__ == "__main__":
+    app.run(port=5001) 
